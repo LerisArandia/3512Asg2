8a2cfb53a7f7f4d1a74d2e35297f6370119384d9
user.name <racus946@mtroyal.ca>
2019-11-15 11:24:03 -0700

    changed html to php

---
+++ favorites.php
@@ -0,0 +1,42 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Favorites</title>
+
+    <link rel="stylesheet" href="css/template.css">
+    <link rel="stylesheet" href="css/favorites.css">
+</head>
+
+<body>
+    <main class="container">
+        <div id="header">
+            <!-- insert logo here -->
+            <!--For Media Query Nav-->
+            <div id="hamburger-menu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul id="navigation">
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Browse/Search</a></li>
+                <li><a href="">Countries</a></li>
+                <li><a href="">Cities</a></li>
+                <li><a href="">Login</a></li>
+                <li><a href="">Sign Up</a></li>
+            </ul>
+        </div>
+        <div class="main">
+            <div id="favorites">
+
+            </div>
+        </div>
+    </main>
+</body>
+
+
+<script src="js/template.js"></script>
+</html>
---
+++ upload.php
@@ -0,0 +1,57 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Upload</title>
+
+    <link rel="stylesheet" href="css/template.css">
+    <link rel="stylesheet" href="css/upload.css">
+    <link href="https://fonts.googleapis.com/css?family=Quicksand&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <main class="container">
+        <div id="header">
+            <!-- insert logo here -->
+            <!--For Media Query Nav-->
+            <div id="hamburger-menu">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <ul id="navigation">
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Browse/Search</a></li>
+                <li><a href="">Countries</a></li>
+                <li><a href="">Cities</a></li>
+                <li><a href="">Login</a></li>
+                <li><a href="">Sign Up</a></li>
+            </ul>
+        </div>
+        <div class="main">
+            <h2>Upload Photo</h2>
+            <div id="upload">
+                <form id="uploadForm" action="lab12-test02.php" method="get">
+                    <label for="title">Title:</label>
+                    <input type="text" placeholder="Enter Title of Picture" name="title" required>
+                    <label for="description">Description:</label>
+                    <input type="textarea" rows="30" name="description" placeholder="Enter Photo's Description" />
+                    <select name="country" id="country">
+                        <option value="">Select Country</option>
+                    </select>
+                    <select name="city" id="city">
+                        <option value="">Select City</option>
+                    </select>
+                    <input type="file" name="pic" accept="image/*">
+                    <button id="submit" type="submit">Upload Your Photo!</button>
+                </form>
+            </div>
+        </div>
+    </main>
+</body>
+
+
+<script src="js/template.js"></script>
+</html>
