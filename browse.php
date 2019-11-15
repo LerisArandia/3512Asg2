2af021fd3f772818d9dfc6ef21c5d08f582f10d7
Jamie <Jamie>
2019-11-15 13:26:46 -0700

    added media query to some php

--- browse.php
+++ browse.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Search/Browse</title>
 
-    <link rel="stylesheet" href="css/browse.css">
     <link rel="stylesheet" href="css/template.css">
     <link rel="stylesheet" href="css/browse.css">
 </head>
