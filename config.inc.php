cb06b08ca601c37573a8d49bbeae0f49571ba37b
racus946 <racus946@mtroyal.ca>
2019-11-27 22:05:51 -0700

    getenv

--- config.inc.php
+++ config.inc.php
@@ -1,7 +1,7 @@
 <?php
 define('DBHOST', 'localhost');
 define('DBNAME', 'travel');
-define('DBUSER', 'root');
+define('DBUSER', getenv('root'));
 define('DBPASS', '');
 define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
 ?>
