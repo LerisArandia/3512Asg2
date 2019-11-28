c9a17d8ab7d983546b227ce696e1c9ba8d35bdf4
racus946 <racus946@mtroyal.ca>
2019-11-27 20:23:45 -0700

    working???

--- index.php
+++ index.php
@@ -28,7 +28,7 @@
                 <li><a href="single-city.php">Cities</a></li>
                 <li><a href="login.php">Login</a></li>
                 <li><a href="signUp.php">Sign Up</a></li>
-                <li><a href="api-cities.php">CITIES API</a></li>
+                <!-- <li><a href="api-cities.php">CITIES API</a></li> -->
                 <li><a href="api-countries.php">COUNTRIES API</a></li>
             </ul>
         </div>
