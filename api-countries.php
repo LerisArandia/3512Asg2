13658469dfe8ef99a1b83fd3d99afea143a76af3
racus946 <racus946@mtroyal.ca>
2019-11-28 19:16:18 -0700

    DB CONN IN STOCK CONFIG

--- api-countries.php
+++ api-countries.php
@@ -10,11 +10,11 @@ header('Content-Type: application/json');
 
 if(isset($_GET['iso'])){
     $iso = $_GET['iso'];
-    $country = getACountry(setConnectionInfo(DBCONNECTION, USER, PASS), $iso);
+    $country = getACountry(setConnectionInfo(DBCONNSTRING, DBUSER, DBPASS), $iso);
     
 }
 else{
-    $country = getAllCountries(setConnectionInfo(DBCONNECTION, USER, PASS));
+    $country = getAllCountries(setConnectionInfo(DBCONNSTRING, DBUSER, DBPASS));
 }
 
 echo $country;
--- includes/stock-config.inc.php
+++ includes/stock-config.inc.php
@@ -20,12 +20,13 @@ define('PASS', $password);
 // auto load all classes so we don't have to explicitly include them
 spl_autoload_register(function ($class) {
     $file = 'lib/' . $class . '.class.php';
+    
     if (file_exists($file)) 
       include $file;
 });
 
 // connect to the database
-$connection = DatabaseHelper::createConnectionInfo(array(DBCONNECTION, DBUSER, DBPASS));
+$connection = DatabaseHelper::createConnectionInfo(array(DBCONNECTION, USER, PASS));
 
 // we can then pass this connection variable to other classes that need it
 
