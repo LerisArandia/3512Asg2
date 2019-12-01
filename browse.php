607b3260ff859d1883f07ea344015c27ca971828
racus946 <racus946@mtroyal.ca>
2019-12-01 15:50:34 -0700

    browse page

--- browse.php
+++ browse.php
@@ -1,3 +1,11 @@
+<?php
+    function display(){
+        if (isset($_POST['continents']) && $_POST['continents']!=""){
+            echo 'Continent selected is: ' . $_POST['continents'];
+        } 
+    }
+?>
+
 <html>
 
 <head>
@@ -30,8 +38,34 @@
             </ul>
         </div>
         <div class="main">
-            <div id="photoFilter">Photo Filter</div>
-            <div id="results">Browse/Search Results</div>
+            <div id="photoFilter">
+            
+            <h3>Photo Filter</h3>
+            
+            <form method=post action=browse.php>
+            
+            <select name="continents" id="continentList">
+                <option value="" >Continents</option>
+                <option value="AF">Africa</option>
+                <option value="AN">Antarctica</option>
+                <option value="AS">Asia</option>
+                <option value="EU">Europe</option>
+                <option value="NA">North America</option>
+                <option value="OC">Oceania</option>
+                <option value="SA">South America</option>
+            </select>
+            <input type="checkbox" class="searchImage" name="countryImages" value="countryImgs">
+            <input type="text" class="search" placeholder="Search by image name">
+
+            <input type='submit' value='post' />
+            </form>
+            </div>
+            
+            <div id="results">
+            <h3>Browse/Search Results </h3>
+
+                <?php display() ?>
+            </div>
         </div>
     </main>
 
