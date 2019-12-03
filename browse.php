3d5345f02ffb203ae0594e5785acd9fb6f26f457
racus946 <racus946@mtroyal.ca>
2019-12-02 20:33:38 -0700

    browse and helper functions

--- browse.php
+++ browse.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
     function display(){
         if (isset($_POST['continents']) && $_POST['continents'] != ""){
             echo 'Continent selected is: ' . $_POST['continents'];
@@ -16,36 +14,17 @@
 ?>
 
 <html>
-
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Search/Browse</title>
-
-    <link rel="stylesheet" href="css/template.css">
+<?php 
+        $title = "Search & Browse";
+        include "includes/head.php";
+    ?>
     <link rel="stylesheet" href="css/browse.css">
 </head>
 
 <body>
     <main class="container">
-        <div id="header">
-            <!-- insert logo here -->
-            <!--For Media Query Nav-->
-            <div id="hamburger-menu">
-                <span></span>
-                <span></span>
-                <span></span>
-            </div>
-            <ul id="navigation">
-                <li><a href="index.php">Home</a></li>
-                <li><a href="about.php">About</a></li>
-                <li><a href="browse.php">Browse/Search</a></li>
-                <li><a href="single-country.php">Countries</a></li>
-                <li><a href="single-city.php">Cities</a></li>
-                <li><a href="login.php">Login</a></li>
-                <li><a href="signUp.php">Sign Up</a></li>
-            </ul>
-        </div>
+    <?php include "includes/navigation.php" ; ?>
         <div class="main">
             <div id="photoFilter">
             
--- helper-functions.inc.php
+++ helper-functions.inc.php
@@ -1,5 +1,8 @@
 <?php
 
+require_once 'config.inc.php';
+require_once 'db-functions.inc.php'; 
+
 function getCountrySql(){
     $sql = 'SELECT ISO, ISONumeric, CountryName, Capital, countries.CityCode, Area, countries.Population, Continent, TopLevelDomain, CurrencyCode, CurrencyName, PhoneCountryCode, Languages, Neighbours, CountryDescription FROM countries';
     return $sql;
@@ -7,6 +10,11 @@ function getCountrySql(){
 
 function getCitySql(){
     $sql = "SELECT cities.CityCode, AsciiName, CountryCodeISO, Latitude, Longitude, cities.Population, Elevation, TimeZone FROM cities";
+    return $sql;
+}
+
+function getLanguageSql(){
+    $sql = "SELECT languages.id, name, languages.iso from languages";
     return $sql;
 }
 
@@ -37,7 +45,7 @@ function getAllCities($connection){
 function getACountry($connection, $iso){
 
     try{
-        $sql = getCountrySql() . ' INNER JOIN cities ON countries.ISO = cities.CountryCodeIso WHERE ISO=?';
+        $sql = getCountrySql() . ' WHERE ISO=?';
         $result = runQuery($connection, $sql, $iso);
 
         return $result;
@@ -51,7 +59,8 @@ function getACountry($connection, $iso){
 function getACity($connection, $cityCode){
 
     try{
-        $sql = getCitySql() . ' WHERE CityCode=?';
+        $sql = getCitySql() . ' WHERE cities.CityCode=?';
+        
         $result = runQuery($connection, $sql, $cityCode);
 
         return $result;
@@ -77,5 +86,47 @@ function getAllCitiesInCountry($connection, $iso){
 
 }
 
+function getALanguage($connection, $iso){
+    
+    try{
+        $sql = getLanguageSql() . " WHERE iso='". $iso."'";
+        $result = $connection->query($sql);
+        $language = $result->fetch();
+        
+        return $language['name'];
+    }
+    catch(PDOException $e){
+        die( $e->getMessage());
+    }
+
+}
+
+function findLanguages($pdo, $languages){
+    $array = explode(",", $languages);
+    $string = "";
+
+    foreach($array as $a){
+        $lang = substr($a, 0, 2);
+        $langName = getALanguage($pdo, $lang);
+        $string .= $langName . ", ";
+    }
+
+    return substr($string, 0, -2);
+}
+
+function findNeighboringCountries($pdo, $neighbours){
+    $array = explode(",", $neighbours);
+    $string = "";
+
+    foreach( $array as $a){
+        $sql = getCountrySql() . " WHERE ISO='" . $a ."'";
+        $result = $pdo->query($sql);
+        $country = $result->fetch();
+
+        $string .= " {$country['CountryName']},";
+    }
+
+    return substr($string, 0, -1); // removes last comma
+}
 
 ?>
