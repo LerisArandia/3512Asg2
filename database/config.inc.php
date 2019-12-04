e36ef19b3bb2569416bc7a18b5068d60b481d17f
Jamie <Jamie>
2019-12-04 11:22:36 -0700

    Changed some things on old config file

--- database/config.inc.php
+++ database/config.inc.php
@@ -3,6 +3,6 @@ define('DBHOST', 'localhost');
 define('DBNAME', 'travel');
 define('DBUSER', 'root');
 define('DBPASS', '');
-define('DBCONNSTRING',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
+define('DBCONNECTION',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
 ?>
 
