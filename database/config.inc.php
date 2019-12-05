971981296ee3ab9aa4bf8af166b1921c592c4fea
Jamie <Jamie>
2019-12-05 16:56:20 -0700

    no message

--- database/config.inc.php
+++ database/config.inc.php
@@ -1,22 +1,20 @@
 <?php
 /******This is for XAMPP******/
-define('DBHOST', 'localhost');
-define('DBNAME', 'travel');
-define('DBUSER', 'root');
-define('DBPASS', '');
-define('DBCONNECTION',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
+// define('DBHOST', 'localhost');
+// define('DBNAME', 'travel');
+// define('DBUSER', 'root');
+// define('DBPASS', '');
+// define('DBCONNECTION',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
 
 /******This is for GCP******/
 // set error reporting on to help with debugging
-/* error_reporting(E_ALL);
-   ini_set('display_errors','1'); */
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
-// // you may need to change these for your own environment
-/* define('DBCONNECTION', getenv('MYSQL_DSN'));
-   define('DBUSER', getenv('MYSQL_USER'));
-   define('DBPASS', getenv('MYSQL_PASSWORD')); */
+// you may need to change these for your own environment
+define('DBCONNECTION', getenv('MYSQL_DSN'));
+define('DBUSER', getenv('MYSQL_USER'));
+define('DBPASS', getenv('MYSQL_PASSWORD'));
 
 //https://comp-3512-asg-2-2019.appspot.com
 //API KEY: AIzaSyDvRG7tyWWBJc3cGLMMfjYsAHFHM-dB7QA
-?>
-
