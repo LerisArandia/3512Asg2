c202fcb0c6a3889489c0666bf507d14f7bad6ad1
racus946 <racus946@mtroyal.ca>
2019-12-08 14:50:15 -0700

    saving form data when registration is not successful

--- signUp.php
+++ signUp.php
@@ -3,7 +3,15 @@ session_start();
 if (isset($_SESSION['email'])) {
     header("location: index-loggedin.php");
 }
+
+if (isset($_GET["email"])){
+    echo $_GET["email"];
+    echo "<br> hello";
+}
 ?>
+
+
+
 <!DOCTYPE html>
 <html>
 
@@ -23,13 +31,13 @@ if (isset($_SESSION['email'])) {
             <h2>Create an Account</h2>
             <div id="register">
                 <form id="registerForm" action="addSignup.php" method="post">
-                    <input type="text" placeholder="First Name" name="fName" required >
-                    <input type="text" placeholder="Last Name" name="lName" required>
-                    <input type="text" placeholder="Country" name="country" required>
-                    <input type="text" placeholder="City" name="city" required>
-                    <input type="email" placeholder="Email" name="email" required>
-                    <input type="password" placeholder="Password" name="password" required>
-                    <input type="password" placeholder="Confirm Password" name="confirm" required>
+                    <input type="text" placeholder="First Name" name="fName" required value=<?= 'ralph'?>> 
+                    <input type="text" placeholder="Last Name" name="lName" required value=<?= 'acusar'?>>
+                    <input type="text" placeholder="Country" name="country" required value=<?= 'canada'?>>
+                    <input type="text" placeholder="City" name="city" required value=<?= 'calgary'?>>
+                    <input type="email" placeholder="Email" name="email" required value=<?= 'racus946@mtroyal.ca'?>>
+                    <input type="password" placeholder="Password" name="password" required value=<?= 'mypassword'?>>
+                    <input type="password" placeholder="Confirm Password" name="confirm" required value=<?= 'mypassword'?>>
                     <button id="signUp" type="submit" name='submit'>Sign Up</button>
                 </form>
             </div>
