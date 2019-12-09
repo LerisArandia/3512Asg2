b0037cc7c187e2e6ccb10c5b1193adf38dc16ee4
LerisArandia <aaran577@mtroyal.ca>
2019-12-08 20:39:36 -0700

    .

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php 
 session_start(); 
 
-if (isset($_SESSION['email'])) {
-    echo $_SESSION['email'];
-    header("Location: index-loggedin.php");
-}
+// if (isset($_SESSION['email'])) {
+//     echo $_SESSION['email'];
+//     header("Location: index-loggedin.php");
+// }
 
 // unset($_SESSION["email"]);
 ?>
