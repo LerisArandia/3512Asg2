1aa35ffeda19f872e39b49608e051628ba8ffea2
Jamie <Jamie>
2019-12-09 14:14:02 -0700

    fixed css for login page.

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 <?php
-    require_once('database/config.inc.php');
-    require_once('includes/session-functions.php');
+require_once('database/config.inc.php');
+require_once('includes/session-functions.php');
 
-    if(isset($_SESSION['email'])){
-        header("location: index-loggedin.php");
-    }
+if (isset($_SESSION['email'])) {
+    header("location: index-loggedin.php");
+}
 
 ?>
 <!DOCTYPE html>
@@ -23,32 +23,30 @@
     <main class="container">
         <?php include "includes/navigation.php"; ?>
         <div class="main">
-            <div id="content">
-                <?php
-                    if(isset($_SESSION["registerGood"])){
-                        $msg = $_SESSION["registerGood"];
-                        echo $msg;
-                        unset($_SESSION["registerGood"]);
-                    }
-                    ?>
-                    <h2>LOGIN</h2>
-                <form id='loginForm' action='load.php' method='post'>
+            <?php
+            if (isset($_SESSION["registerGood"])) {
+                $msg = $_SESSION["registerGood"];
+                echo $msg;
+                unset($_SESSION["registerGood"]);
+            }
+            ?>
+            <h2>LOGIN</h2>
+            <form id='loginForm' action='load.php' method='post'>
 
                 <?php
-                    if(isset($login_status) && false == $login_status) : ?>
+                if (isset($login_status) && false == $login_status) : ?>
                     <div class="error">
                         <p>Your username and/or password are incorrect. Please try again!</p>
                     </div>
                 <?php endif; ?>
 
-                    <input type="text" id="email" placeholder="Email" name="email" required>
-                    <input type="password" id="password" placeholder="Password" name="password" required>
+                <input type="text" id="email" placeholder="Email" name="email" required>
+                <input type="password" id="password" placeholder="Password" name="password" required>
 
-                    <!-- <button type=button action=""id="cancel">CANCEL</button> -->
-                    <a id="cancel" href="index.php">CANCEL</a>
-                    <button type=submit id="login">LOGIN</button>
-                </form>
-            </div>
+                <!-- <button type=button action=""id="cancel">CANCEL</button> -->
+                <a id="cancel" href="index.php">CANCEL</a>
+                <button type=submit id="login">LOGIN</button>
+            </form>
         </div>
     </main>
     <script src="js/template.js"></script>
