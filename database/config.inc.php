59813cde7809433eef6782bfab811f92c5d16be5
jaewong <35346461+jaeemoo@users.noreply.github.com>
2019-12-09 23:01:58 -0700

    Fixing config file for GCP

--- database/config.inc.php
+++ database/config.inc.php
@@ -1,15 +1,10 @@
 <?php
 /******Configueration to connect to Google Cloud Platform SQL Database******/
-// //set error reporting on to help with debugging
-// error_reporting(E_ALL);
-// ini_set('display_errors', '1');
+// set error reporting on to help with debugging
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
-// define('DBCONNECTION', getenv('MYSQL_DSN'));
-// define('DBUSER', getenv('MYSQL_USER'));
-// define('DBPASS', getenv('MYSQL_PASSWORD'));
-define('DBHOST', 'localhost');
-define('DBNAME', 'travel');
-define('DBUSER', 'root');
-define('DBPASS', '');
-define('DBCONNECTION',"mysql:host=" . DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;");
+define('DBCONNECTION', getenv('MYSQL_DSN'));
+define('DBUSER', getenv('MYSQL_USER'));
+define('DBPASS', getenv('MYSQL_PASSWORD'));
 ?>
