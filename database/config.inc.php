c54ab774af68bee3bf85cd06d3647275c423faa2
jaewong <35346461+jaeemoo@users.noreply.github.com>
2019-12-10 00:03:15 -0700

    changed config

--- database/config.inc.php
+++ database/config.inc.php
@@ -1,16 +1,16 @@
 <?php
 /******Configueration to connect to Google Cloud Platform SQL Database******/
 // set error reporting on to help with debugging
-// error_reporting(E_ALL);
-// ini_set('display_errors', '1');
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
 
-// define('DBCONNECTION', getenv('MYSQL_DSN'));
-// define('DBUSER', getenv('MYSQL_USER'));
-// define('DBPASS', getenv('MYSQL_PASSWORD'));
+define('DBCONNECTION', getenv('MYSQL_DSN'));
+define('DBUSER', getenv('MYSQL_USER'));
+define('DBPASS', getenv('MYSQL_PASSWORD'));
 
-define('DBHOST', 'localhost');
-define('DBNAME', 'travel');
-define('DBUSER', 'root');
-define('DBPASS', '');
-define('DBCONNECTION', "mysql:host=" .DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;" );
+// define('DBHOST', 'localhost');
+// define('DBNAME', 'travel');
+// define('DBUSER', 'root');
+// define('DBPASS', '');
+// define('DBCONNECTION', "mysql:host=" .DBHOST . ";dbname=" . DBNAME . ";charset=utf8mb4;" );
 ?>
